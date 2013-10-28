3da624fbe961bbda5c28a9af8ccd034e085ad1d5
Andrea Biancini <andrea.biancini@gmail.com>
2013-10-28 11:37:45 +0100

    Solved problem for first question

---
+++ android/happymeteo/src/com/happymeteo/QuestionImpulseActivity.java
@@ -0,0 +1,170 @@
+package com.happymeteo;
+
+import org.jraf.android.backport.switchwidget.Switch;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.graphics.Typeface;
+import android.util.Log;
+import android.view.Gravity;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
+import android.widget.TextView;
+
+import com.happymeteo.models.SessionCache;
+import com.happymeteo.models.User;
+import com.happymeteo.utils.Const;
+import com.happymeteo.widget.AppyMeteoSeekBar;
+import com.happymeteo.widget.OnAppyMeteoSeekBarChangeListener;
+
+public abstract class QuestionImpulseActivity extends ImpulseActivity {
+
+	protected LinearLayout linearLayout = null;
+	protected JSONObject questions = null;
+	
+	protected void writeQuestionText(String questionText) {
+		LinearLayout.LayoutParams llp = new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+		llp.setMargins(10, 10, 10, 0);
+		llp.gravity = Gravity.CENTER;
+		
+		int gender = SessionCache.getGender(getApplicationContext());
+		if (gender == User.GENDER_MALE) questionText = questionText.replaceAll("\\[o/a\\]", "o");
+		else questionText = questionText.replaceAll("\\[o/a\\]", "a");
+		
+		TextView textView = new TextView(getApplicationContext());
+		textView.setText(questionText);
+		textView.setLayoutParams(llp);
+		textView.setBackgroundColor(getResources().getColor(R.color.white));
+		textView.setTextColor(getResources().getColor(R.color.black));
+//		textView.setBackgroundResource(R.drawable.fascia);
+		textView.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.CENTER_VERTICAL);
+		textView.setTextSize(23.0f);
+		
+		try {
+			Typeface billabong = Typeface.createFromAsset(getAssets(), "billabong.ttf");
+			textView.setTypeface(billabong);
+		} catch (Exception e) {
+			Log.e(Const.TAG, e.getMessage(), e);
+		}
+
+		linearLayout.addView(textView);
+		
+		LinearLayout.LayoutParams llp2 = new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+		llp2.setMargins(10, 0, 10, 10);
+		llp2.gravity = Gravity.CENTER;
+		ImageView fascetta = new ImageView(getApplicationContext());
+		fascetta.setLayoutParams(llp2);
+		fascetta.setImageDrawable(getResources().getDrawable(R.drawable.fascia));
+		linearLayout.addView(fascetta);
+	}
+	
+	protected void writeQuestionAnswerArea(final int type, final String id_question, final String textYes, final String textNo) throws JSONException {
+		if (type == 1) {
+			drawOneToTenAnswer(id_question);
+			questions.put(id_question, "1");
+		} else {
+			drawYesNoAnswer(id_question, textYes, textNo);
+			questions.put(id_question, "0");
+		}
+	}
+	
+	protected void drawYesNoAnswer(final String id_question, final String textYes, final String textNo) {
+		LinearLayout.LayoutParams llp = new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+		llp.setMargins(10, 10, 10, 10);
+		
+		LinearLayout linearLayout1 = new LinearLayout(getApplicationContext());
+		linearLayout1.setOrientation(LinearLayout.HORIZONTAL);
+		linearLayout1.setGravity(Gravity.CENTER);
+
+		TextView textYesView = new TextView(this);
+		textYesView.setText(textYes);
+		textYesView.setLayoutParams(llp);
+		linearLayout1.addView(textYesView);
+
+		final Switch switchButton = new Switch(this);
+		switchButton.setLayoutParams(llp);
+		switchButton.setChecked(true);
+		switchButton.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				try {
+					questions.put(id_question, isChecked ? "0" : "1");
+				} catch (JSONException e) {
+					Log.e(Const.TAG, e.getMessage(), e);
+				}
+			}
+		});
+
+		linearLayout1.addView(switchButton);
+
+		TextView textNoView = new TextView(this);
+		textNoView.setText(textNo);
+		textNoView.setLayoutParams(llp);
+		linearLayout1.addView(textNoView);
+
+		linearLayout.addView(linearLayout1);
+	}
+
+	protected void drawOneToTenAnswer(final String id_question) {
+		LinearLayout.LayoutParams llpImg = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+		llpImg.weight = 10;
+		llpImg.gravity = Gravity.CENTER_VERTICAL;
+
+		LinearLayout linearLayout1 = new LinearLayout(this);
+		linearLayout1.setOrientation(LinearLayout.HORIZONTAL);
+
+		ImageView imageView1 = new ImageView(this);
+		imageView1.setImageResource(R.drawable.triste);
+		imageView1.setLayoutParams(llpImg);
+		linearLayout1.addView(imageView1);
+
+		LinearLayout.LayoutParams llp_seekBar = new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+		llp_seekBar.weight = 80;
+
+		final AppyMeteoSeekBar appyMeteoSeekBar = new AppyMeteoSeekBar(this);
+		appyMeteoSeekBar.setMax(90);
+		appyMeteoSeekBar.setProgress(0);
+		appyMeteoSeekBar.setLayoutParams(llp_seekBar);
+		
+		final TextView tvText = new TextView(this);
+		tvText.setText("1\u00B0");
+		tvText.setBackgroundResource(R.drawable.baloon);
+		tvText.setGravity(Gravity.CENTER);
+		tvText.setTextColor(getResources().getColor(R.color.white));
+		tvText.setTextSize(15.0f);
+		LinearLayout.LayoutParams llpBaloon = new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+		llpBaloon.leftMargin = appyMeteoSeekBar.getProgressPosX();
+		tvText.setLayoutParams(llpBaloon);
+		linearLayout.addView(tvText);
+		
+		appyMeteoSeekBar.setOnAppyMeteoSeekBarChangeListener(new OnAppyMeteoSeekBarChangeListener() {
+			@Override
+			public void onProgressPosXChanged(AppyMeteoSeekBar seekBar, int progress, int progressPosX) {
+				String value = String.valueOf((progress / 10) + 1);
+				try {
+					questions.put(id_question, value);
+				} catch (JSONException e) {
+					Log.e(Const.TAG, e.getMessage(), e);
+				}
+				tvText.setText(value + "\u00B0");
+				
+				LinearLayout.LayoutParams llpBaloon = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+				llpBaloon.leftMargin = progressPosX;
+				tvText.setLayoutParams(llpBaloon);
+			}
+		});
+
+		linearLayout1.addView(appyMeteoSeekBar);
+
+		ImageView imageView2 = new ImageView(this);
+		imageView2.setImageResource(R.drawable.felice);
+		imageView2.setLayoutParams(llpImg);
+		linearLayout1.addView(imageView2);
+
+		linearLayout.addView(linearLayout1);
+	}
+
+}
