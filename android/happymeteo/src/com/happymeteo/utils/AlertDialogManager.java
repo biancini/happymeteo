6c94eba266dcc1bd8046334279f47a70b76ef3d5
Simon Vocella <voxsim@gmail.com>
2013-11-07 18:35:33 +0100

    fix bug in AlertDialogManager: not show alert when the activity is finishing

--- android/happymeteo/src/com/happymeteo/utils/AlertDialogManager.java
+++ android/happymeteo/src/com/happymeteo/utils/AlertDialogManager.java
@@ -8,38 +8,44 @@ import com.happymeteo.R;
 
 public class AlertDialogManager {
 	static public void showError(Activity activity, String error) {
-		new AlertDialog.Builder(activity)
-		.setTitle(activity.getString(com.happymeteo.R.string.error))
-		.setMessage(error)
-		.setPositiveButton(activity.getString(com.happymeteo.R.string.ok), new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dialog, int which) {
-				// Do nothing
-			}
-		})
-		.setIcon(R.drawable.fail)
-		.show();
+		if(!activity.isFinishing()) {
+			new AlertDialog.Builder(activity)
+			.setTitle(activity.getString(com.happymeteo.R.string.error))
+			.setMessage(error)
+			.setPositiveButton(activity.getString(com.happymeteo.R.string.ok), new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int which) {
+					// Do nothing
+				}
+			})
+			.setIcon(R.drawable.fail)
+			.show();
+		}
 	}
 	
 	static public void showErrorAndRetry(Activity activity, String error, DialogInterface.OnClickListener retryClickListener) {
-		new AlertDialog.Builder(activity)
-		.setTitle(activity.getString(com.happymeteo.R.string.error))
-		.setMessage(error)
-		.setPositiveButton(activity.getString(com.happymeteo.R.string.retry), retryClickListener)
-		.setNegativeButton(activity.getString(com.happymeteo.R.string.cancel), new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dialog, int which) {
-				// Do nothing
-			}
-		})
-		.setIcon(R.drawable.fail)
-		.show();
+		if(!activity.isFinishing()) {
+			new AlertDialog.Builder(activity)
+			.setTitle(activity.getString(com.happymeteo.R.string.error))
+			.setMessage(error)
+			.setPositiveButton(activity.getString(com.happymeteo.R.string.retry), retryClickListener)
+			.setNegativeButton(activity.getString(com.happymeteo.R.string.cancel), new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int which) {
+					// Do nothing
+				}
+			})
+			.setIcon(R.drawable.fail)
+			.show();
+		}
 	}
 	
 	static public void showNotification(Activity activity, int title, int message, DialogInterface.OnClickListener clickListener) {
-		new AlertDialog.Builder(activity)
-		.setTitle(activity.getString(title))
-		.setMessage(activity.getString(message))
-		.setPositiveButton(activity.getString(com.happymeteo.R.string.ok), clickListener)
-		.setIcon(R.drawable.success)
-		.show();
+		if(!activity.isFinishing()) {
+			new AlertDialog.Builder(activity)
+			.setTitle(activity.getString(title))
+			.setMessage(activity.getString(message))
+			.setPositiveButton(activity.getString(com.happymeteo.R.string.ok), clickListener)
+			.setIcon(R.drawable.success)
+			.show();
+		}
 	}
 }
