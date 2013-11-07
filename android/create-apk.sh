522d768e2a0623ec6e6b8c2b7e6b616786a8a72a
Simon Vocella <voxsim@gmail.com>
2013-11-07 18:35:57 +0100

    zipalign in create-apk and check ANDROID_HOME env

--- android/create-apk.sh
+++ android/create-apk.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
-echo "Clean & Build with Gradle..."
-./gradlew clean build
+if env | grep ANDROID_HOME
+then
+	echo "Clean & Build with Gradle..."
+	./gradlew clean build
 
-echo "Sign the jar..."
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore release/appymeteo.keystore happymeteo/build/apk/happymeteo-release-unsigned.apk appymeteo
+	echo "Sign the jar..."
+	jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore release/appymeteo.keystore happymeteo/build/apk/happymeteo-release-unsigned.apk appymeteo
 
-cp happymeteo/build/apk/happymeteo-release-unsigned.apk release/appymeteo.apk 
-echo "Done."
+	echo "Align signed jar..."
+	$ANDROID_HOME/tools/zipalign -f -v 4 happymeteo/build/apk/happymeteo-release-unsigned.apk release/appymeteo.apk
+
+	echo "Done."
+else
+	echo "You have to set the ANDROID_HOME environment variable."
+fi
