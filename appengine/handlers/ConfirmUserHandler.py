6ca819124eb9f3f5cb00ef4d254ba447bcd348e7
Andrea Biancini <andrea.biancini@gmail.com>
2014-05-03 13:51:57 +0200

    Modified template rendering

--- appengine/handlers/ConfirmUserHandler.py
+++ appengine/handlers/ConfirmUserHandler.py
@@ -2,6 +2,7 @@
 @author: Simon Vocella <voxsim@gmail.com>
 '''
 from google.appengine.ext import db
+from google.appengine.ext.webapp import template
 import webapp2
 
 class ConfirmUserHandler(webapp2.RequestHandler):
@@ -19,7 +20,9 @@ class ConfirmUserHandler(webapp2.RequestHandler):
            user.status = 2
            user.confirmation_code = ""
            user.put()
-           self.render('confirm_user.html', template_vars={
+
+           path = os.path.join(os.path.dirname(__file_), '..', 'templates', 'confirm_user.html')
+           template.render(path, template_vars={
             'name': user.first_name,
             'email': user.email})
         else:
