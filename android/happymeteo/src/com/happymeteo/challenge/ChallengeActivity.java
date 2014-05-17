fbed71765c1978ca5d6937ddf0fdf6b3782e7256
Andrea Biancini <andrea.biancini@gmail.com>
2014-05-17 13:13:42 +0200

    Fixed image for opponent in challenge score activity

--- android/happymeteo/src/com/happymeteo/challenge/ChallengeActivity.java
+++ android/happymeteo/src/com/happymeteo/challenge/ChallengeActivity.java
@@ -111,7 +111,7 @@ public class ChallengeActivity extends LoggedActivity {
 					Bundle extras = new Bundle();
 					extras.putString(ChallengeScoreActivity.IO_CHALLENGE, ioScore.toString());
 					extras.putString(ChallengeScoreActivity.TU_CHALLENGE, tuScore.toString());
-					extras.putString(ChallengeScoreActivity.TU_FACEBOOK_ID, challenge.getAdversary().getUser_id());
+					extras.putString(ChallengeScoreActivity.TU_FACEBOOK_ID, challenge.getAdversary().getFacebook_id());
 					extras.putString(ChallengeScoreActivity.TU_NAME, challenge.getAdversary().getFirst_name());
 					extras.putString(ChallengeScoreActivity.HAS_CLOSE, "true");
 					invokeActivity(ChallengeScoreActivity.class, extras);
